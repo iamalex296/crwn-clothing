defc01bbba3f2743ddf81068483d06f8ba44b677
alex <alexizana10@gmail.com>
2021-03-16 01:40:15 +0400

    adding styles to options

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 import { auth } from '../../firebase/firebase.utils'
 
-import { ReactComponent as HomePageLogo } from '../../assets/crown.svg'
+import { ReactComponent as HomePageLogo } from '../../assets/home.svg'
 
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
-    <Link className='logo-container' to="/">
-      <HomePageLogo className='logo' />
+    <Link style={{padding: '2px 0px'}} className='logo-container' to="/">
+      <HomePageLogo style={{width: '50px', height: '50px', opacity: '80%', padding: '0px'}} className='logo' />
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
